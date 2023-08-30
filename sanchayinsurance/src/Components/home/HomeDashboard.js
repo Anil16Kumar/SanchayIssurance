167bda54f2bc4bc50f8064c64f8d1538d5cc41ae
Akashmodi371 <Akashmodi371@gmail.com>
2023-08-30 21:13:13 +0530

    added carousel to home

--- sanchayinsurance/src/Components/home/HomeDashboard.js
+++ sanchayinsurance/src/Components/home/HomeDashboard.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './HomeDashboard.css'
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../../assets/Carousel2.jpg'
+import img2 from '../../assets/carousel1.jpg'
+import { Image } from 'react-bootstrap'
 
 const HomeDashboard = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
   return (
-    <div>HomeDashboard</div>
+    <div className='container'>
+    <div className='header'>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <Image src={img1} fluid/>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <Image src={img2} fluid/>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <Image src={img2} fluid/>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+    <div className='content'></div>
+    <div className='footer'></div>
+    </div>
   )
 }
 
